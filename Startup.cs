640c580d6c0361b7811cfb345102b362af679f72
mobileMartonis <mobile@martonis.com.br>
2019-09-05 10:31:51 -0300

    Adicionado Swagger para documentar a API

--- Startup.cs
+++ Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using ProductCatalog.Data;
 using ProductCatalog.Repositories;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace ProductCatalog
 {
@@ -13,14 +14,22 @@ namespace ProductCatalog
         {
             services.AddMvc();
 
+            //Comprime todas as requisições para depois enviar para qualquer lugar 
+            services.AddResponseCompression(); 
+
             // Cria um item apenas 
             // AddScoped verifica se existe algum Datacontext na memória e se não existir 
             // ele me dá um novo!
             services.AddScoped<StoreDataContext, StoreDataContext>();
 
-            // É add transiente pq toda vez que eu pedir um ProductRepository eu quero uma nova instância 
+            // É add transient pq toda vez que eu pedir um ProductRepository eu quero uma nova instância 
             // do ProductRepository
             services.AddTransient<ProductRepository, ProductRepository>();
+
+            //Adiciona o Swagger para documentação da API
+            services.AddSwaggerGen(x => {
+                x.SwaggerDoc("v1", new Info {Title = "ProductCatalog", Version = "v1"});
+            });
         }
 
         
@@ -30,6 +39,14 @@ namespace ProductCatalog
                 app.UseDeveloperExceptionPage();
 
                 app.UseMvc();
+
+                app.UseResponseCompression();
+
+                app.UseSwagger();
+
+                app.UseSwaggerUI(c => {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "ProductCatalog - V1");
+                });
         }   
     }
 }
